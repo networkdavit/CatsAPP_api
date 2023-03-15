7210c8a9b012ba335c978f3041a5f4d34429f718
networkdavit <lonecreation1612@gmail.com>
2023-03-15 11:54:01 +0400

    New schemas

--- seeders/seed.js
+++ seeders/seed.js
@@ -7,12 +7,13 @@ const jsonData = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
 
 const breeds = jsonData.breeds;
 const pieces_of_advice = jsonData.pieces_of_advice;
+const food = jsonData.food;
 async function seedBreeds() {
     const db = new sqlite3.Database('../database.db');
     db.run("DELETE FROM breeds")
     for (const breed of breeds) {
         await new Promise((resolve, reject) => {
-          db.run('INSERT INTO breeds (id, title, description, age, origin, isFavorite, pros, cons, imageURL) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', [breed.id, breed.title, breed.description, breed.age, breed.origin, breed.isFavorite, breed.pros, breed.cons, breed.imageURL], (error) => {
+          db.run('INSERT INTO breeds (id, title, description, age, origin, isFavorite, pros, cons, imageURL, advice) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?,?)', [breed.id, breed.title, breed.description, breed.age, breed.origin, breed.isFavorite, breed.pros, breed.cons, breed.imageURL, breed.advice], (error) => {
             if (error) {
               reject(error);
             } else {
@@ -43,5 +44,24 @@ async function seedAdvice() {
       db.close();
 }
 
+async function seedFood() {
+    const db = new sqlite3.Database('../database.db');
+    db.run("DELETE FROM food")
+    for (const cat_food of food) {
+        await new Promise((resolve, reject) => {
+          db.run('INSERT INTO food (id, brand, cat_type, reason, imageURL) VALUES (?, ?, ?,?,?)', [cat_food.id, cat_food.brand, cat_food.cat_type, cat_food.reason, cat_food.imageURL], (error) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve();
+            }
+          });
+        });
+      }
+    
+      db.close();
+}
+
 seedBreeds();
 seedAdvice();
+seedFood();
