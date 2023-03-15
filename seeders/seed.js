5d625dce89df1d98a7de047810cf20a2c93c8d68
networkdavit <lonecreation1612@gmail.com>
2023-03-15 21:59:26 +0400

    Refactoring files, schema and logic for retrieving quiz data

--- seeders/seed.js
+++ seeders/seed.js
@@ -8,6 +8,9 @@ const jsonData = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
 const breeds = jsonData.breeds;
 const food = jsonData.food;
 const interactive_games = jsonData.interactive_games;
+const quiz_questions = jsonData.quiz_questions;
+const quiz_answers = jsonData.quiz_answers;
+const quiz_results = jsonData.quiz_results;
 
 async function seedBreeds() {
     const db = new sqlite3.Database('../database.db');
@@ -63,6 +66,63 @@ async function seedFood() {
       db.close();
 }
 
+async function seedQuizQuestions() {
+  const db = new sqlite3.Database('../database.db');
+  db.run("DELETE FROM quiz_questions")
+  for (const question of quiz_questions) {
+      await new Promise((resolve, reject) => {
+        db.run('INSERT INTO quiz_questions (id, question, imageURL) VALUES (?, ?, ?)', [question.id, question.question, question.imageURL], (error) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve();
+          }
+        });
+      });
+    }
+  
+    db.close();
+}
+
+async function seedQuizAnswers() {
+  const db = new sqlite3.Database('../database.db');
+  db.run("DELETE FROM quiz_answers")
+  for (const answer of quiz_answers) {
+      await new Promise((resolve, reject) => {
+        db.run('INSERT INTO quiz_answers (id, answer, question_id) VALUES (?, ?, ?)', [answer.id,answer.answer, answer.question_id], (error) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve();
+          }
+        });
+      });
+    }
+  
+    db.close();
+}
+
+async function seedQuizResults() {
+  const db = new sqlite3.Database('../database.db');
+  db.run("DELETE FROM quiz_results")
+  for (const result of quiz_results) {
+      await new Promise((resolve, reject) => {
+        db.run('INSERT INTO quiz_results (id, answer_pattern, imageURL) VALUES (?, ?, ?)', [result.id, result.answer_pattern, result.imageURL], (error) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve();
+          }
+        });
+      });
+    }
+  
+    db.close();
+}
+
 seedBreeds();
 seedGames();
 seedFood();
+seedQuizQuestions();
+seedQuizAnswers();
+seedQuizResults();
